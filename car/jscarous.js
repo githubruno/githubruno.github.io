7365252aef041bdc25d7d775d8e7dc3378b61992
Github Runo <githubruno@poige.ru>
2015-07-10 20:34:53 +0700

    text and styles added

--- car/jscarous.js
+++ car/jscarous.js
@@ -1,5 +1,23 @@
-$('.carousel').carousel({
+var main=function(){
+	$('.carousel').carousel({
   interval: false
 });
-
+}
 $(document).ready(main);
+
+var menu=function(){
+	$('nav').find('li').click(function(){
+		$('nav').find('li').removeClass('active');
+		$(this).addClass('active');
+	});
+}
+$(document).ready(menu);
+
+
+
+
+
+
+
+
+
